6c47351cf121746ef2e26eaf3af575033825917b
Devianina Tanya <120410818+Tanya02898@users.noreply.github.com>
2024-05-01 00:38:13 +0300

    Add files via upload

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"domain","l":"MatrixCalculation"},{"p":"domain","l":"MatrixMain"},{"p":"domain","l":"MatrixOutput"}];updateSearchResults();
